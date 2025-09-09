2e4f9b6e34d9783d039458edfca643937fef7f44
pixel-autometa <online.class081125@gmail.com>
2025-09-09 23:47:43 +0600

    Update config.js

--- frontend/config.example.js
+++ frontend/config.example.js
@@ -1,4 +1,3 @@
-// Copy this file to `config.js` and replace the BACKEND_URL with your deployed FastAPI URL.
 window.CONFIG = {
-  BACKEND_URL: "http://127.0.0.1:8000"
+  BACKEND_URL: "https://sajid-portfolio-backend.onrender.com/"
 };
